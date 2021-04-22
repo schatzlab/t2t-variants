d900551fe7797a8672600eb2ffc95f1c8ad127fe
Mike Schatz <mschatz@cs.jhu.edu>
2021-04-22 11:26:06 -0400

    automatically create pdfs

--- chromosome_repeats/chromosome_repeats.R
+++ chromosome_repeats/chromosome_repeats.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(scales)
 
 setwd("/Users/mschatz/Dropbox/Documents/Projects/T2T/t2t-variants/chromosome_repeats")
+dir.create("plot")
 
 ## chm13 
 ###############################################################################
@@ -16,8 +17,9 @@ chm$chr = factor(chm$chr, chm$chr)
 chm_long <- pivot_longer(chm, cols=2:4, names_to="type", values_to="len")
 chm_long$sample <- "CHM13"
 
+pdf("plot/chm13_chromosome_uniqueness.pdf", width=11, height=8.5)
 ggplot(chm_long, aes(x=chr, y=len)) + geom_col(aes(fill=type)) + ggtitle("CHM13 Chromosome Uniquenes (75-mers)") + theme(plot.title = element_text(hjust = 0.5))
-
+dev.off()
 
 ## hg38
 ###############################################################################
@@ -30,15 +32,18 @@ hg38$chr = factor(hg38$chr, hg38$chr)
 hg38_long <- pivot_longer(hg38, cols=2:4, names_to="type", values_to="len")
 hg38_long$sample <- "GRCh38"
 
+pdf("plot/grch38_chromosome_uniqueness.pdf", width=11, height=8.5)
 ggplot(hg38_long, aes(x=chr, y=len)) + geom_col(aes(fill=type)) + ggtitle("GRCh38 Chromosome Uniquenes (75-mers)") + theme(plot.title = element_text(hjust = 0.5))
-
+dev.off()
 
 ## genome comparison
 ###############################################################################
 
 all_long <- rbind(hg38_long, chm_long)
-ggplot(all_long, aes(x=sample, y=len)) + geom_col(aes(fill=type)) + facet_grid(~chr) + theme(axis.text.x = element_text(angle = 90)) + ggtitle("Chromosome Uniquenes (75-mers)") + theme(plot.title = element_text(hjust = 0.5))
 
+pdf("plot/cmp_chromosome_uniqueness.pdf", width=11, height=8.5)
+ggplot(all_long, aes(x=sample, y=len)) + geom_col(aes(fill=type)) + facet_grid(~chr) + theme(axis.text.x = element_text(angle = 90)) + ggtitle("Chromosome Uniquenes (75-mers)") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
 
 
 
@@ -48,21 +53,28 @@ ggplot(all_long, aes(x=sample, y=len)) + geom_col(aes(fill=type)) + facet_grid(~
 colors=hue_pal()(3)
 
 extra_unique = data.frame(chr=hg38$chr, len=chm$unique-hg38$unique)
-ggplot(extra_unique, aes(x=chr, y=len)) + geom_col(fill=colors[3]) + labs(title="Additional Unique Bases per Chromosome (75 mers)") + theme(plot.title = element_text(hjust = 0.5))
 sum(extra_unique$len)
 
+pdf("plot/cmp_chromosome_extra_unique.pdf", width=11, height=8.5)
+ggplot(extra_unique, aes(x=chr, y=len)) + geom_col(fill=colors[3]) + labs(title="Additional Unique Bases per Chromosome (75 mers)") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
 
 ## extra total bases per chromosome
 ###############################################################################
 
 extra_total = data.frame(chr=hg38$chr, len=chm$total-hg38$total)
 sum(extra_total$len)
-ggplot(extra_total, aes(x=chr, y=len)) + geom_col(fill=colors[3]) + labs(title="Chromosome length differences (all bases)") + theme(plot.title = element_text(hjust = 0.5))
 
+pdf("plot/cmp_chromosome_extra_bases.pdf", width=11, height=8.5)
+ggplot(extra_total, aes(x=chr, y=len)) + geom_col(fill=colors[3]) + labs(title="Chromosome length differences (all bases)") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
 
 ## extra non-n
 ###############################################################################
 
 extra_nonn = data.frame(chr=hg38$chr, len=(chm$total-chm$n)-(hg38$total-hg38$n))
 sum(extra_nonn$len)
+
+pdf("plot/cmp_chromosome_extra_nonn.pdf", width=11, height=8.5)
 ggplot(extra_nonn, aes(x=chr, y=len)) + geom_col(fill=colors[3]) + labs(title="Chromosome length differences (non-n bases)") + theme(plot.title = element_text(hjust = 0.5))
+dev.off()
